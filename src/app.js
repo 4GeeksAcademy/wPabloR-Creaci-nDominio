beb18a532016678b879eef3725392b2ea75c9ea6
wPabloR <ricotep0@gmail.com>
2025-05-14 18:09:16 +0000

    create doms

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let pronoun = ['el', 'nuestro', 'tu'];
+  let adj = ['gran', 'pequeño', `ridículo`];
+  let noun = ['lagarto', 'ciempiés', `gato`];
+  let doms =[];
+
+  function createDOM(arr1, arr2, arr3){
+    for (let char1 of arr1){
+        for (let char2 of arr2){
+            for (let char3 of arr3){
+                doms.push(char1 + char2 + char3 + `.com`)
+            }
+        }   
+  }
+    return doms
+}
+
+  console.log(createDOM(pronoun, adj, noun));
 };
